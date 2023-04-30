b64b46b9475843de7f5506140ec91cd28cc99b49
nitrolego <47659924+Nitrolego@users.noreply.github.com>
2023-04-30 11:58:05 +0800

    added renaming

--- misamix.py
+++ misamix.py
@@ -1,1 +1,15 @@
-print("hellow wowwd")
+#imports
+import os
+
+#determining file path
+absolute_path = os.path.dirname(__file__)
+before_path = "before/perfectclear.txt"
+after_path = "after/sfx_perfectclear.txt"
+full_before_path = os.path.join(absolute_path, before_path)
+full_after_path = os.path.join(absolute_path, after_path)
+
+os.rename(full_before_path, full_after_path)
+print("renamed")
+
+#if no dir, make dir (default/sfx)
+
