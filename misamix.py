18cc0cda99cbd888428353ea8d0199c4cf29ea7d
nitrolego <47659924+Nitrolego@users.noreply.github.com>
2023-05-08 20:11:04 +0800

    added before folder to git

--- misamix.py
+++ misamix.py
@@ -8,9 +8,12 @@ from os.path import exists, join, dirname
 #file path for folder for Misamino
 absolute_path = dirname(__file__)
 src_folder = join(absolute_path, "sfx/default")
+bfr_folder = join(absolute_path, "before")
 new_folder = join(absolute_path, "after/sfx/default")
-if not exists(new_folder):
-    makedirs(new_folder)
+req_folder = [bfr_folder, new_folder]
+for folder in req_folder:
+    if not exists(folder):
+        makedirs(folder)
 
 #function to simplify exporting to WAV files
 def export_to_wav_from_ogg(path, misaname):
@@ -44,36 +47,42 @@ convertDict['allclear'] = ['sfx_perfectclear']
 for i in range(1,17):
     convertDict[f'combo_{i}'] = [f'sfx_combo{i}']
 
-#Looping through all ogg files in repository to convert to WAV
-oggpaths = [path for path in Path(absolute_path).rglob("*.ogg")]
-mp3paths = [path for path in Path(absolute_path).rglob("*.mp3")]
-wavpaths = [path for path in Path(absolute_path).rglob("*.wav")]
+#Looping through all ogg,mp3, and wav files in before folder
+oggpaths = [path for path in Path(bfr_folder).rglob("*.ogg")]
+mp3paths = [path for path in Path(bfr_folder).rglob("*.mp3")]
+wavpaths = [path for path in Path(bfr_folder).rglob("*.wav")]
 
 allpaths = oggpaths + mp3paths + wavpaths
 
-#refactored processing of all file formats
-for path in allpaths:
-    print(path.name)
-    for originalnames, misanames in convertDict.items():
-        for sfx in misanames:
-            if path.name == f'{originalnames}.ogg':
-                export_to_wav_from_ogg(path, f'{sfx}')
-                continue
-            elif path.name == f'{originalnames}.mp3':
-                export_to_wav_from_mp3(path, f'{sfx}')
-                continue
-            elif path.name == f'{originalnames}.wav':
-                copy(path, join(new_folder, f'{sfx}.wav'))
-                continue
-            elif not exists(join(new_folder, f'{sfx}.wav')):
-                copy(join(src_folder, f'{sfx}.wav'), new_folder)
-                continue
+#if folder is not empty, run logic
+if allpaths:
+    #refactored processing of all file formats
+    for path in allpaths:
+        print(path.name)
+        for originalnames, misanames in convertDict.items():
+            for sfx in misanames:
+                if path.name == f'{originalnames}.ogg':
+                    export_to_wav_from_ogg(path, f'{sfx}')
+                    continue
+                elif path.name == f'{originalnames}.mp3':
+                    export_to_wav_from_mp3(path, f'{sfx}')
+                    continue
+                elif path.name == f'{originalnames}.wav':
+                    copy(path, join(new_folder, f'{sfx}.wav'))
+                    continue
+                elif not exists(join(new_folder, f'{sfx}.wav')):
+                    copy(join(src_folder, f'{sfx}.wav'), new_folder)
+                    continue
 
-#The following sounds are not avaliable in most tetrio soundpacks
-copy(join(src_folder, "sfx_combo17.wav"), new_folder)
-copy(join(src_folder, "sfx_combo18.wav"), new_folder)
-copy(join(src_folder, "sfx_combo19.wav"), new_folder)
-copy(join(src_folder, "sfx_combo20.wav"), new_folder)
-copy(join(src_folder, "sfx_lockdown.wav"), new_folder)
-copy(join(src_folder, "sfx_movefail.wav"), new_folder)
-copy(join(src_folder, "sfx_rotatefail.wav"), new_folder)
+    #The following sounds are not avaliable in most tetrio soundpacks
+    copy(join(src_folder, "sfx_combo17.wav"), new_folder)
+    copy(join(src_folder, "sfx_combo18.wav"), new_folder)
+    copy(join(src_folder, "sfx_combo19.wav"), new_folder)
+    copy(join(src_folder, "sfx_combo20.wav"), new_folder)
+    copy(join(src_folder, "sfx_lockdown.wav"), new_folder)
+    copy(join(src_folder, "sfx_movefail.wav"), new_folder)
+    copy(join(src_folder, "sfx_rotatefail.wav"), new_folder)
+#else, usually when the folder is empty or that the before folder is deleted.
+else:
+    print('empty folder, quitting')
+    quit()
