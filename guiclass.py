bbcc1ce36385fbbfdf6775a5e95c9253958b6569
nitrolego <47659924+Nitrolego@users.noreply.github.com>
2023-05-16 20:22:25 +0800

    refactored guiclass

--- guiclass.py
+++ guiclass.py
@@ -9,19 +9,10 @@ from misaclass import MisaClass
 
 class MisaGUI:
     def __init__(self, root) -> None:
-        #variable declaration
-        self.absolute_path = dirname(__file__)
-        self.bfr_filepath = join(self.absolute_path, "before")
-        self.new_filepath = join(self.absolute_path, "after/sfx/default")
-
         # name of window
         root.title("MisaMix")
         # do not allow window resizing
         root.resizable(False, False)
-        #root.geometry("550x150")
-        
-
-        
 
         #mainframe to prevent widget off colour
         mainframe = ttk.Frame(root, padding="3 3 12 12")
@@ -29,41 +20,37 @@ class MisaGUI:
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
-        #before folder labels and button
-        self.descbfr_lbl = ttk.Label(mainframe, text="Input:")
-        self.descbfr_lbl.grid(column=0, row=0, sticky=(N, W))
-        
+        #variable declaration
+        self.absolute_path = dirname(__file__)
+        self.bfr_filepath = join(self.absolute_path, "before")
+        self.new_filepath = join(self.absolute_path, "after/sfx/default")
         self.bfr_text = StringVar(value=self.bfr_filepath)
-
-        self.bfr_lbl = ttk.Label(mainframe, textvariable=self.bfr_text, width=60)
-        self.bfr_lbl.grid(column=1, row=0, sticky=(N, W))
-
-        self.bfr_button = ttk.Button(mainframe, text="Open",
-                                        command=self.openbfrdir)
-        self.bfr_button.grid(column=2, row=0, sticky=(N, W))
-        
-
-        #new folder labels and button
-        self.descnew_lbl = ttk.Label(mainframe, text="Output:")
-        self.descnew_lbl.grid(column=0, row=1, sticky=(N, W))
-
         self.new_text = StringVar(value=self.new_filepath)
-
-        self.new_lbl = ttk.Label(mainframe, textvariable=self.new_text, width=60)
-        self.new_lbl.grid(column=1, row=1, sticky=(N, W))
-
-        self.new_button = ttk.Button(mainframe, text="Open",
-                                    command=self.opennewdir)
-        self.new_button.grid(column=2, row=1, sticky=(N, W))
-
-        #success label
         self.success_text = StringVar()
-        self.success_lbl = ttk.Label(mainframe, textvariable=self.success_text)
-        self.success_lbl.grid(column=1, row=3, sticky=S)
-        
 
-        #run button
-        self.run_button = ttk.Button(mainframe, text="Run", command=self.main)
+        #label declaration
+        self.descbfr_lbl = ttk.Label(mainframe, text="Input:")
+        self.bfr_lbl = ttk.Label(mainframe, textvariable=self.bfr_text, width=60)
+        self.descnew_lbl = ttk.Label(mainframe, text="Output:")
+        self.new_lbl = ttk.Label(mainframe, textvariable=self.new_text, width=60)
+        self.success_lbl = ttk.Label(mainframe, textvariable=self.success_text)
+
+        #button declaration
+        self.bfr_btn = ttk.Button(mainframe, text="Open",
+                                        command=self.openbfrdir)
+        self.new_btn = ttk.Button(mainframe, text="Open",
+                                        command=self.opennewdir)
+        self.run_btn = ttk.Button(mainframe, text="Run", 
+                                        command=self.main)
+
+        #grid positioning of widgets
+        self.descbfr_lbl.grid(column=0, row=0, sticky=(N, W))
+        self.bfr_lbl.grid(column=1, row=0, sticky=(N, W))
+        self.bfr_button.grid(column=2, row=0, sticky=(N, W))        
+        self.descnew_lbl.grid(column=0, row=1, sticky=(N, W))
+        self.new_lbl.grid(column=1, row=1, sticky=(N, W))
+        self.new_button.grid(column=2, row=1, sticky=(N, W))
+        self.success_lbl.grid(column=1, row=3, sticky=S)
         self.run_button.grid(column=3, row=3, sticky=(E))
 
         for child in mainframe.winfo_children(): 
